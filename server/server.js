adbc40ed5f7d93c3ea177f8baf12e9426965a67d
Gulshan2589 <87886553+Gulshan2589@users.noreply.github.com>
2023-02-27 16:52:01 +0530

    server

--- server/server.js
+++ server/server.js
@@ -20,14 +20,8 @@ app.use('/api/transaction/', transactionRoute);
 const port = process.env.PORT || 4000;
 
 
-if (process.env.NODE_ENV === 'production') 
-{
-    app.use('/', express.static('client/build'));
-    // Define a simple route to test the server
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-    })
-}
+// Define a simple route to test the server
+app.get('/', (req,res) => res.send('Hello World'));
 
 
 // Start the server and listen for incoming requests on the specified port
