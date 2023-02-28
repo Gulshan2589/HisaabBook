e5a6a0e1107c7f211a8bd8af711e1e9d467c24ba
Gulshan2589 <87886553+Gulshan2589@users.noreply.github.com>
2023-02-28 12:26:54 +0530

    server c

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ const port = process.env.PORT ||  4000;
 
 if(process.env.NODE_ENV === 'production')
 {
-     app.use('/' , express.static('client/build'))
+     app.use('/' , express.static('./client/build'))
 
      app.get('*' , (req, res)=>{
-         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+         res.sendFile(path.resolve(__dirname, './client/build/index.html'))
      })
 }
 
