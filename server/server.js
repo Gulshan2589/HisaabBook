3c8225de92c583c60b760ba847ca38392b90476b
Gulshan2589 <87886553+Gulshan2589@users.noreply.github.com>
2023-02-28 12:38:43 +0530

    server c

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ const dbConnect = require('./dbConnect')
 // Create an instance of Express.js
 const app = express()
 
-
-
 // Middleware to parse incoming requests as JSON
 app.use(express.json())
 
@@ -23,10 +21,10 @@ const port = process.env.PORT ||  4000;
 
 if(process.env.NODE_ENV === 'production')
 {
-     app.use('/' , express.static('./client/build'))
+     app.use('/' , express.static('../client/build'))
 
      app.get('*' , (req, res)=>{
-         res.sendFile(path.resolve(__dirname, './client/build/index.html'))
+         res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
      })
 }
 
