07a2afc2572a904d256c41becb7062bb3581b4a8
Gulshan2589 <87886553+Gulshan2589@users.noreply.github.com>
2023-02-28 12:41:57 +0530

    server c

--- server/server.js
+++ server/server.js
@@ -17,27 +17,14 @@ app.use('/api/users/', userRoute);
 app.use('/api/transaction/', transactionRoute);
 
 // Set the port to listen for incoming requests
-const port = process.env.PORT ||  4000;
+const port = 4000;
 
-if(process.env.NODE_ENV === 'production')
-{
-     app.use('/' , express.static('../client/build'))
-
-     app.get('*' , (req, res)=>{
-         res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
-     })
-}
-
-// app.use(express.static(path.join(__dirname, 'client/build')));
-// // Define a simple route to test the server
-// app.get('*', function(req, res){
-//     res.sendFile(path.join(__dirname, 'client/build/index.html'));
-// });
-
-
+// Define a simple route to test the server
+app.get('/', (req,res) => res.send('Hello World'));
 
 // Start the server and listen for incoming requests on the specified port
 app.listen(port, () => console.log(`Node JS Server started at port ${port}!`));
+
 
 
 
