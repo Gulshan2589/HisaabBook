08d40c4c5e108d5b60cde4cad08a8046c30e9491
Gulshan2589 <87886553+Gulshan2589@users.noreply.github.com>
2023-03-05 17:08:21 +0530

    build

--- server.js
+++ server.js
@@ -16,17 +16,16 @@ const transactionRoute = require('./routes/transactionRoute');
 app.use('/api/users/', userRoute);
 app.use('/api/transaction/', transactionRoute);
 
-const path = require('path');
 
 // Set the port to listen for incoming requests
 const port = process.env.PORT ||  4000;
 
 if(process.env.NODE_ENV === 'production')
 {
-     app.use('/' , express.static('../client/build'))
+     app.use('/' , express.static('client/build'))
 
      app.get('*' , (req, res)=>{
-         res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
      })
 }
 
